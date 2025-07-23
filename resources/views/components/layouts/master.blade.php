8381e508d5da2349f2d854f465ce5b564e4644da
Jerry <azhagirishankar5@gmail.com>
2025-07-23 14:41:26 +0530

    #2 updated layout

--- resources/views/components/layouts/master.blade.php
+++ resources/views/components/layouts/master.blade.php
@@ -1,21 +1,25 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
     @vite('resources/css/app.css')
     <script src="//unpkg.com/alpinejs" defer></script>
-
 </head>
+
 <body>
 <div>
-    <x-partials.nav />
+    <x-partials.nav/>
 </div>
+
 <div>
     {{$slot}}
 </div>
+
 </body>
 </html>
